121b455fbb789e145c1194e65642a898fc0d6dd0
virajrodge5 <virajrodge5@gmail.com>
2024-03-12 01:38:07 +0530

    Mobile screens styling change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div
           className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
         >
-          <div className="flex flex-col justify-center items-center mt-5">
+          <div className="hidden md:flex flex-col justify-center items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
             <div className="w-1 sm:h-80 h-80 violet-gradient" />
           </div>
