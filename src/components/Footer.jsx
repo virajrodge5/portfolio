0f4276c62408028ed6fc0047ee8c41aedcd252b6
virajrodge5 <virajrodge5@gmail.com>
2024-03-12 13:13:55 +0530

    Footer font change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
 import SocialMediaIcons from "./SocialMediaIcons";
 
+import { styles } from "../styles";
+
 const Footer = () => {
   return (
     <footer className="h-25 bg-primary pt-5 pb-10">
       <div className="w-10/12 mx-auto flex flex-col justify-center items-center">
         <SocialMediaIcons />
         <div className="text-center">
-            <p className="font-playfair font-semibold text-2xl text-white">
-              VIRAJ RODGE
-            </p>
+          <p className={`${styles.sectionSubText}`}>
+            MADE BY{"   "}
+            <span className="text-2xl font-playfair font-semibold md:text-3xl text-white">
+                VIRAJ RODGE
+            </span>
+          </p>
         </div>
       </div>
     </footer>
